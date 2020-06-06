d087ae85fd6eb41d3e52f2e6f89f8554be2513cd
Xiaobo <1362596047@qq.com>
2020-06-06 20:14:41 +0800

    文件上传

---
+++ uploadfiles/src/main/java/com/pl1111w/uploadfiles/UploadfilesApplication.java
@@ -0,0 +1,13 @@
+package com.pl1111w.uploadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadfilesApplication.class, args);
+    }
+
+}
---
+++ uploadfiles/src/test/java/com/pl1111w/uploadfiles/UploadfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl1111w.uploadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
